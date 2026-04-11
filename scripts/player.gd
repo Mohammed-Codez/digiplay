1a4b0090ccff6c5f43afc27817f6645ce98b96d5
Mohammed-Codez <amakactive@gmail.com>
2026-04-11 18:23:30 +0400

    added more tiles and shaders

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,25 @@ extends CharacterBody2D
 @export var max_speed := 1600.0
 @export var jump_height := -360.0
 
-@onready var sprite = $AnimatedSprite2D
+@onready var sprite := $AnimatedSprite2D
+@onready var coyote := $CoyoteTimer
 
 func _physics_process(delta: float) -> void:
+	var direction := 0.0
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		velocity.x *= 0.9
+		coyote.start()
+		
+	direction = Input.get_axis("move_left", "move_right")
+	velocity.x *= 0.85
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and\
+	(is_on_floor() or not coyote.is_stopped()):
 		velocity.y = jump_height
 
-	var direction := Input.get_axis("move_left", "move_right")
-	
 	velocity.x += direction * max_speed * delta 
-	velocity.x *= 0.9
 
 	move_and_slide()
 
